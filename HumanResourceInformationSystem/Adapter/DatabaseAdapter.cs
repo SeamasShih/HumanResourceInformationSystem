30f62ede2180144548da7b55f4546710ecb11214
SeamasShih <seamas0716@gmail.com>
2020-09-27 13:05:31 +1000

    Add empty database adapter

---
+++ HumanResourceInformationSystem/Adapter/DatabaseAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourceInformationSystem.Adapter
+{
+    public class DatabaseAdapter
+    {
+       // create static function in here
+    }
+}
