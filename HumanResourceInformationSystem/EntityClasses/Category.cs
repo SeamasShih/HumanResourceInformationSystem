ff464dfef27bb184a46f032348b5af5da516fcef
xuanthangdong1992 <71828695+xuanthangdong1992@users.noreply.github.com>
2020-09-28 07:53:17 +1000

    Add staff entity

---
+++ HumanResourceInformationSystem/EntityClasses/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourceInformationSystem.EntityClasses
+{
+    public enum Category
+    {
+        Academic = 0,
+        Technical = 1,
+        Administrative = 2,
+        Casual = 3
+    }
+}
---
+++ HumanResourceInformationSystem/EntityClasses/Staff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourceInformationSystem.EntityClasses
+{
+    public class Staff
+    {
+        public int Id { private set; get; }
+        public string GivenName { private set; get; }
+        public string FamilyName { private set; get; }
+        public string Title { private set; get; }
+        public Campus Campus { private set; get; }
+        public string Phone { private set; get; }
+        public string Room { private set; get; }
+        public string Email { private set; get; }
+        public string Photo { private set; get; }
+        public Category Category { private set; get; }
+    }
+}
